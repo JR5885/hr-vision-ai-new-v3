1b6cb61d57b9318e11c5087b87e7e9a289b31552
JR5885 <149295047+JR5885@users.noreply.github.com>
2026-08-29 16:14:01 +0800

    Update next.config.mjs

--- hr-vision-ai/next.config.mjs
+++ hr-vision-ai/next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  productionBrowserSourceMaps: false,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
